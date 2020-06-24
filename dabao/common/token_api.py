5e0bd233cb0f3a49aaf1bcc0d2af51d2a039db92
litchixie <38877046+litchixie@users.noreply.github.com>
2020-06-24 15:51:15 +0800

    Update token_api.py

--- dabao/common/token_api.py
+++ dabao/common/token_api.py
@@ -6,7 +6,7 @@ import json
 
 def token():
     s = requests.session()
-    r = s.get("http://192.168.14.228:1337/api/v1/token")
+    r = s.get("URL")
     toekn = r.text
     toekn = r.json()["token"]
     return toekn
